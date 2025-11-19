5cbb0d7eb25ed065e348ad2ca899cda8359931b7
Anthony-Vdls <a.compsci@proton.me>
2025-11-19 16:49:01 -0700

    added page 5

---
+++ pages/5_Network_Visualization.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Page Config ###########################################
+
+
+
+
+# Data ##################################################
+
+data = [
+        ('Alice', 'Bob'),
+        ('Alice','Charlie'),
+        ('Bob','Charlie'),
+        ('Charlie','Diana'),
+        ('Diana','Eve'),
+        ('Bob','Diana'),
+        ('Frank','Eve'),
+        ('Eve','Ian'),
+        ('Diana','Ian'),
+        ('Ian','Grace'),
+        ('Grace','Hannah'),
+        ('Hannah','Jack'),
+        ('Grace','Jack'),
+        ('Charlie','Frank'),
+        ('Alice','Eve'),
+        ('Bob','Jack'),
+        ]
+
+g = nx.Graph()
+g.add_edges_from(data)
+pos = nx.spring_layout(g)  # Force-directed layout
+nx.draw(g, pos, with_labels=True, node_color='lightgreen', edge_color='gray')
+plt.show()
